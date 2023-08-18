e079b953db8438db9bf53e7575db8a2d4294adb2
Lucas Abraao <Labraao957@gmail.com>
2023-08-18 15:50:11 -0300

    Adicionar arquivos de projeto.

---
+++ Models/EmprestimosModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int id { get; set; }
+        public string recebedor { get; set; }
+        public string fornecedor { get; set; }
+        public string livroEmprestado { get; set; }
+        public DateTime dataUltimaAtualizacao { get; set; } =  DateTime.Now;
+
+    }
+}
